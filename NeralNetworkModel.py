16a6992607c0e0749595a4affc6e33bb0676a9af
zyle-geralde <zylgerdlp19@gmail.com>
2024-07-29 00:49:27 +0800

    initial commit

---
+++ NeralNetworkModel.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import tensorflow as tf
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.activations import linear, relu, sigmoid
+np.set_printoptions(edgeitems=2000, linewidth=2000)
+
